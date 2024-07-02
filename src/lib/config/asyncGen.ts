a369b605d178172a30ccd9da6c1dc9b60f0972d9
affantaufiqur <affantaufiqur@gmail.com>
2024-07-02 21:06:44 +0700

    create async generator

---
+++ src/lib/config/asyncGen.ts
@@ -0,0 +1,23 @@
+export default async function* sendRequest(url: string, options: RequestInit = {}) {
+	yield { status: 'fetching', data: null };
+	const req = await fetch(url, options);
+	yield { status: 'received', data: req };
+	const parsed = await req.json();
+	yield { status: 'parsed', data: parsed };
+}
+
+for await (const req of sendRequest('https://jsonplaceholder.typicode.com/todos/1')) {
+	switch (req.status) {
+		case 'fetching':
+			console.log('fetching...');
+			break;
+		case 'received':
+			for (const [k, v] of req.data.headers) {
+				console.log(k, v);
+			}
+			break;
+		case 'parsed':
+			console.log('parsed', req.data);
+			break;
+	}
+}
