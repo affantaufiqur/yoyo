5bf2c50c27b793ed68de294613bb0db3f2c1e8d4
affantaufiqur <affantaufiqur@gmail.com>
2024-07-05 09:48:45 +0700

    remove optmimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	optimizeDeps: {
-		exclude: [
-			'codemirror',
-			'@codemirror/lang-javascript',
-			'@codemirror/view',
-			'@codemirror/autocomplete',
-			'@codemirror/state',
-			'@codemirror/lang-json',
-			'@replit/codemirror-vim'
-		]
-	}
+	plugins: [sveltekit()]
 });
