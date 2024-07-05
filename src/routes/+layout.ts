357dcce823bada8b558dc881517db8e2e8b66656
affantaufiqur <affantaufiqur@gmail.com>
2024-07-05 10:57:29 +0700

    added posthog

---
+++ src/routes/+layout.ts
@@ -0,0 +1,13 @@
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
+import { env } from '$env/dynamic/public';
+
+export const load = async () => {
+	if (browser) {
+		posthog.init(env.PUBLIC_POSTHOG_URI!, {
+			api_host: 'https://us.i.posthog.com',
+			person_profiles: 'always'
+		});
+	}
+	return;
+};
